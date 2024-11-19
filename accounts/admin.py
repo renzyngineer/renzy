5a55c9dc8ca51f86ad714b560c0decee0d6e9f7b
Renzy <142571301+renzyngineer@users.noreply.github.com>
2024-11-19 14:28:49 +0800

    Initial commit

---
+++ accounts/admin.py
@@ -0,0 +1,30 @@
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = [
+        "email",
+        "username",
+    ]
+
+    # Explicitly define add_fieldsets to prevent unexpected fields
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "email", "password1", "password2"),
+            },
+        ),
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
